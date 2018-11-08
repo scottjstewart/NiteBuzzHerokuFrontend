62459d2c734438da53b8d537a4bf203dc4b274fb
scottjstewart <scottstewart965@gmail.com>
2018-11-08 15:50:11 -0500

    front end heroku push cloned

--- src/app/intercept.url.ts
+++ src/app/intercept.url.ts
@@ -1,16 +1,24 @@
-import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler } from "@angular/common/http";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpEvent,
+  HttpHandler
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-const base = 'http://localhost:3000'
+const base = "http://nitebuzz2018v1.herokuapp.com";
 
 @Injectable()
 export class UrlInterceptor implements HttpInterceptor {
-    constructor() { }
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        req = req.clone({
-            url: base + req.url
-        })
-        return next.handle(req)
-    }
+  constructor() {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      url: base + req.url
+    });
+    return next.handle(req);
+  }
 }
